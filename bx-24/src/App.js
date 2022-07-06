3d3ff2457884307306fe108810dc64c473c164af
yegor-web <lotov.97@list.ru>
2022-07-06 15:10:37 +0300

    bx-24

---
+++ bx-24/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AddTask } from './AddTask';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Добавление задачи</h1>
+      <AddTask />
+    </div>
+  );
+};
+
+export default App;
